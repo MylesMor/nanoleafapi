787ddf0571b5ee50b41c41402e8aa7d3bfb9784d
MylesMor <mylesmor@gmail.com>
2019-12-30 02:31:50 +0000

    Fix package

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nanoleaf-py", # Replace with your own username
+    name="nanoleafapi", # Replace with your own username
     version="1.0.0",
     author="Myles Morgan",
     author_email="hello@mylesmor.dev",
-    description="A Python wrapper for the Nanoleaf OpenAPI," +
+    description="A Python 3 wrapper for the Nanoleaf OpenAPI," +
                  "for use when controlling both Light Panels and Canvas.",
     long_description=long_description,
     long_description_content_type="text/markdown",
