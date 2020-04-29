893b803de8091a235eba741c11a6a5ab7a696723
Myles <mylesmor@gmail.com>
2020-04-29 23:55:18 +0100

    Update discovery.py
    - Resolve error described in issue #4
    - Add a debug argument to `discover_devices()` that prints out the SSDP string

--- nanoleafapi/discovery.py
+++ nanoleafapi/discovery.py
@@ -2,11 +2,12 @@
 
 import socket
 
-def discover_devices(timeout=30):
+def discover_devices(timeout=30, debug=False):
     """
     Discovers Nanoleaf devices on the network using SSDP
 
     :param timeout: The timeout on the search in seconds (default 30)
+    :param debug: Prints each device string in for the SSDP discovery
     :returns: Dictionary of found devices in format {name: ip}
     """
     ssdp = "M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN: \"ssdp:discover\"\r\nMX: 1\r\nST: nanoleaf:nl29\r\n\r\n"
@@ -27,11 +28,16 @@ def discover_devices(timeout=30):
     nanoleaf_dict = {}
 
     for device in nanoleaves:
+        if debug:
+            print(device)
         headers = device.split('\r\n')
+        ip = None
+        name = None
         for header in headers:
             if "Location" in header:
                 ip = header.split("http://")[1][:-6]
             if "nl-devicename" in header:
                 name = header.split("nl-devicename: ")[1]
-        nanoleaf_dict[name] = ip
+        if ip is not None:
+            nanoleaf_dict[name] = ip
     return nanoleaf_dict
